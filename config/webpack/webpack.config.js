686192c7257b255544efcdf4a5a215ea94985103
Lucia S <self@lucia.cr>
2025-03-02 02:03:01 -0600

    TS y Sass y otras cosillas tal vez? No me acuerdo

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,9 +1,13 @@
 // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
 const { generateWebpackConfig, inliningCss } = require('shakapacker');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const ForkTSCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
-const webpackConfig = generateWebpackConfig();
+const webpackConfig = generateWebpackConfig({
+  plugins: [new ForkTSCheckerWebpackPlugin()],
+});
 
 if (isDevelopment && inliningCss) {
   webpackConfig.plugins.push(
